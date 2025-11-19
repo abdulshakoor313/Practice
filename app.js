81c29174142965a6e092a3252cd09d7111ea656b
Abdul Shakoor <ibupoto@gmail.com>
2025-11-20 03:37:12 +0500

    ....

--- app.js
+++ app.js
@@ -13,3 +13,40 @@ let z= 40;
 if(x<=12){
     console.log("haaaaa")
 }
+let ab = 56;
+if(ab%2 ===0){
+    console.log("It is Even");
+}
+else{
+    console.log("It is Odd");
+}
+let ac = -5;
+if(ac>0){
+    console.log("positive")
+}
+else{
+console.log("Negative");
+}
+let marks = 40;
+if(marks>=33){
+    console.log("pass")
+}
+else{
+    console.log("Fail");
+}
+let age = 16;
+if(age>=18){
+    console.log("Eligible")
+}
+else{
+    console.log("not Eligible")
+}
+
+let num1 = 12;
+let num2 = 18;
+if(num1>num2){
+    console.log("1 is greater than ");
+}
+else{
+    console.log("2 is greater than")
+}
