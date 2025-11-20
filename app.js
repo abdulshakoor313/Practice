2c442114165665a81f0fd059b4de36d964709b98
Abdul Shakoor <ibupoto@gmail.com>
2025-11-21 01:37:01 +0500

    .....

--- app.js
+++ app.js
@@ -1,52 +1,95 @@
-let a = 12;
+// let a = 12;
+// let b = 12;
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// let n = -5;
+// console.log(n);
+// let x = 10;
+// let y= 25;
+// let z= 40;
+
+// if(x<=12){
+//     console.log("haaaaa")
+// }
+// let ab = 56;
+// if(ab%2 ===0){
+//     console.log("It is Even");
+// }
+// else{
+//     console.log("It is Odd");
+// }
+// let ac = -5;
+// if(ac>0){
+//     console.log("positive")
+// }
+// else{
+// console.log("Negative");
+// }
+// let marks = 40;
+// if(marks>=33){
+//     console.log("pass")
+// }
+// else{
+//     console.log("Fail");
+// }
+// let age = 16;
+// if(age>=18){
+//     console.log("Eligible")
+// }
+// else{
+//     console.log("not Eligible")
+// }
+
+// let num1 = 12;
+// let num2 = 18;
+// if(num1>num2){
+//     console.log("1 is greater than ");
+// }
+// else{
+//     console.log("2 is greater than")
+// }
+
+// let a = 12;
+// let b = 18;
+// let c = 15;
+// if(a>b && a>c){
+//     console.log("A is greater than Every number");
+// }
+// else if(b>a && b>c){
+//     console.log("B is Greater Than Every number");
+// }
+// else if(c>a && c>b){
+//     console.log("C is greater than Every number");
+// }
+
+// let a = 14;
+// let b = 7;
+// let c = 22;
+
+// if(a<b && a<c){
+//     console.log("A is smallest No");
+// }
+// else if(b<a && b<c){
+//     console.log("B is Smallest No");
+// }
+// else {
+//     console.log("C is Smallest No");
+// }
+
+let a = 18;
 let b = 12;
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-let n = -5;
-console.log(n);
-let x = 10;
-let y= 25;
-let z= 40;
-
-if(x<=12){
-    console.log("haaaaa")
-}
-let ab = 56;
-if(ab%2 ===0){
-    console.log("It is Even");
+let c = 12;
+// if(a==b && a==c){
+//     console.log("All are Equal");
+// }
+// else{
+//     console.log("Not all eqaul");
+// }
+if(b==a && b==c){
+    console.log("All equal");
 }
 else{
-    console.log("It is Odd");
-}
-let ac = -5;
-if(ac>0){
-    console.log("positive")
-}
-else{
-console.log("Negative");
-}
-let marks = 40;
-if(marks>=33){
-    console.log("pass")
-}
-else{
-    console.log("Fail");
-}
-let age = 16;
-if(age>=18){
-    console.log("Eligible")
-}
-else{
-    console.log("not Eligible")
-}
-
-let num1 = 12;
-let num2 = 18;
-if(num1>num2){
-    console.log("1 is greater than ");
-}
-else{
-    console.log("2 is greater than")
-}
+    console.log("Not eqaul")
+};
