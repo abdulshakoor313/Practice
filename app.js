99bf4e684bba13350cf0069cf3cbd46cf9b9b34d
Abdul Shakoor <ibupoto@gmail.com>
2025-11-23 22:49:19 +0500

    .....

--- app.js
+++ app.js
@@ -78,18 +78,168 @@
 //     console.log("C is Smallest No");
 // }
 
-let a = 18;
-let b = 12;
-let c = 12;
+// let a = 18;
+// let b = 12;
+// let c = 12;
 // if(a==b && a==c){
 //     console.log("All are Equal");
 // }
 // else{
 //     console.log("Not all eqaul");
 // }
-if(b==a && b==c){
-    console.log("All equal");
+
+// let a = 18;
+// let b = 12;
+// let c = 12;
+
+
+// if(b==a && b==c){
+//     console.log("All equal");
+// }
+// else{
+//     console.log("Not eqaul");
+// }
+// if(a>b && a>c){
+//     console.log("A is greater than Every Number");
+// }
+// else if(b>a && b>c){
+//     console.log("B is greater than Every Number");
+// }
+// else("C is greater than every Number");
+
+
+
+
+// if(a<b && a<c){
+//     console.log("A is less than Every Number ");
+// }
+// else if(b<a && b<c){
+//     console.log("B is less than Everey Number");
+// }
+// else{
+//     console.log("C is less than Every Number");
+// };
+
+
+
+// if (a == b) {
+//     console.log("A is equal to B");
+// }
+// else if (a == c) {
+//     console.log("A is equal to C");
+// }
+// else if (b == c) {
+//     console.log("B is equal to C");
+// }
+// else {
+//     console.log("No numbers are equal");
+// }
+
+// const newdate = new Date();
+// console.log(newdate.getFullYear());
+
+
+
+// let num = 15;
+
+// if(num>0)
+//     {
+//     console.log("Num is Posistive");
+//     if(num%2===0){
+//         console.log("Num is Even")
+//     }
+//     else{
+//         console.log("Num is Odd")
+//     }
+// }
+// else if(num<0){
+//     console.log("Num is negative");
+//     if(num%2===0){
+//         console.log("Num is Even")
+//     }
+//     else{
+//         console.log("Num is Odd")
+//     }
+// }
+// else{
+//     console.log(" Num is 0");
+// }
+
+
+// let num = 12;
+
+// if(num!=0){
+//     console.log("Num is not 0")
+//     if(num>0){
+//         console.log("Num is Positive")
+//         if(num%2===0){
+//             console.log("Num is positive Even")
+//         }
+//         else{
+//             console.log("Num is positive Odd")
+//         }
+//         if(num%3===0){
+//             console.log("Num Divisibe by 3 Completely")
+//         }
+//         else{
+//             console.log("Num is not divisible by 3 completely")
+//         }
+//         if(num%5===0){
+//             console.log("Num Divisibe by 5 Completely")
+//         }
+//         else{
+//             console.log("Num is not divisible by 5 completely")
+//         }
+//     }
+//     else{
+//         if(num<0){
+//             console.log("Num is Negative")
+//         }
+//         if(num%2 ===0){
+//             console.log("Num is negative Even")
+//         }
+//         else{
+//             console.log("Num is negative Oddd")
+//         }
+//         if(num%3===0){
+//             console.log("Num Divisibe by 3 Completely")
+//         }
+//         else{
+//             console.log("Num is not divisible by 3 completely")
+//         }
+//         if(num%5===0){
+//             console.log("Num Divisibe by 5 Completely")
+//         }
+//         else{
+//             console.log("Num is not divisible by 5 completely")
+//         }
+//     }
+    
+// }
+// else{
+//     console.log("Num is 0 could not performed more operations ")
+
+// }
+ let num = 16;
+
+if(num>0){
+    console.log("Num is positive")
+    if(num>10 && num<50){
+        console.log("Positive and in range 10-50")
+    }
+    else if(num<10 && num>0){
+        console.log("Positive and in range 0-10")
+    }
+
+}else if(num<0){
+    console.log("Num is Negative")
+    if(num<-10 && num<-50){
+        console.log("Negative, and in range -10 to -50")
+    }else{
+        console.log("Just for console....")
+    }
 }
 else{
-    console.log("Not eqaul")
-};
+    console.log("Num is 0")
+
+}
